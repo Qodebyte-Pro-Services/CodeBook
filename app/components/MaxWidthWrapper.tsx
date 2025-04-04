c861dbdd74be152462585dfe887690407b5f8f0c
Benson-xix <bensondominic81@gmail.com>
2025-04-04 12:55:06 +0300

    Add all project files

--- app/components/MaxWidthWrapper.tsx
+++ app/components/MaxWidthWrapper.tsx
@@ -7,7 +7,7 @@ interface MaxWidthWrapperProps {
 }
 
 const MaxWidthWrapper: React.FC<MaxWidthWrapperProps> = ({className, children}) => {
-  return <div className={`lg:mx-auto md:mx-2 max-w-[1920px]  xl:px-20 md:px-2 ${className}`}>
+  return <div className={`lg:mx-auto md:mx-2 max-w-[1920px]  xl:px-1 md:px-2 ${className}`}>
     {children}
   </div>
 };
